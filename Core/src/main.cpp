a9ebc3017a271959663b6a1b3391de433f3f0806
Hanleys <40015515+HanleysFramer@users.noreply.github.com>
2023-12-01 18:24:38 -0600

    Revert "Added barebones parser to dev branch"
    This reverts commit fd500749dbfbd21e0cbfb59c9fb3cced7885d32f.

--- Core/src/main.cpp
+++
@@ -1,9 +0,0 @@
-// This file contains the 'main' entry point function. //
-#include "DefsReader.h"
-
-int main()
-{
-    /* Input a file path (decompressed or compressed) to parse. Populate ADefHandler object with all child nodes. */
-    ADefHandler ratFile("C:/Users/wauke/Desktop/102.adefs");   
-}
-
